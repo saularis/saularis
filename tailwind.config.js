4b04094a0fabea55e13c0b19f67e14f0eb31685b
Abdulla Malik <hi@abdulla.dev>
2023-04-07 17:17:06 +0600

    new home

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
   content: ["./**/*.html"],
   theme: {
+    screens: {
+      'xs': '475px',
+      ...defaultTheme.screens,
+    },
+    fontFamily: {
+      mono: ["JetBrains Mono", "monospace"],
+    },
     extend: {},
   },
   plugins: [],
